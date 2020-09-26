2764e2863a2b02a27e8678568066b94ecb17a704
kix696 <Kiadyrajwels.dark@gmail.com>
2020-09-26 22:57:44 +0200

    ncommente anle rest api fa efa azo amzay

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -19,9 +19,9 @@ class Api extends RestController {
             ['id' => 1, 'name' => 'Jim', 'email' => 'jim@example.com'],
         ];
 
-        $id = $this->get( 'id' );
+        $id = $this->get( 'id' ); //ty le '.../id/2' dia ilay id amny url get
 
-        if ( $id === null )
+        if ( $id === null ) //raha tsisy tohiny oe '/id/..' ilay url dia ireo users rehetra izany no avereno
         {
             // Check if the users data store contains users
             if ( $users )
@@ -40,9 +40,9 @@ class Api extends RestController {
         }
         else
         {
-            if ( array_key_exists( $id, $users ) )
+            if ( array_key_exists( $id, $users ) ) //raha nisy '.../id/...' ilay url ka mi-existe ilay id anaty liste anle users
             {
-                $this->response( $users[$id], 200 );
+                $this->response( $users[$id], 200 ); //averiny ilay user manana ilay id am url
             }
             else
             {
